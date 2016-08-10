f1414d4670c859f1a75cebf0a2dfa08aec3e0f48
Garrett Price and Nhat Hoang <garrett@>
2016-08-10 13:43:53 -0700

    Added more click function buttons to the calculator functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,35 @@
-alert("First line of scripts")
-// // Add
+
 var add = function(number1, number2) {return number1 + number2;};
-// // var number1 = parseInt(prompt("Enter a number:"));
-// // var number2 = parseInt(prompt("Enter another number:"));
-// debugger;
-// // alert(add(number1, number2));
-// // Subtract
+
 var subtract = function(number1, number2) {return number1 - number2;};
-// // var number1 = parseInt(prompt("Enter a number:"));
-// // var number2 = parseInt(prompt("Enter another number:"));
-// // Multiply
+
 var multiply = function(number1, number2) {return number1 * number2;};
-// // var number1 = parseInt(prompt("Enter a number."));
-// // var number2 = parseInt(prompt("Enter anther number."));
-// // Divide
+
 var divide = function(number1, number2) {return number1 / number2;};
-// // var number1 = parseInt (prompt("Enter a number."));
-// // var number2 = parseInt (prompt("Enter another number."));
-// //Result
-// var result = add(number1, number2);
-// // alert(result);
-//
+
 $(document).ready(function() {
   $("form#add").submit(function(event){
-    event.preventDefault();
+    // event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
+    // $("#output").text(result);
+  // });
+  $("button#addbutton").click(function(){
     var result = add(number1, number2);
     $("#output").text(result);
   });
+  $("button#subtractbutton").click(function(){
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("button#multiplybutton").click(function(){
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("button#dividebutton").click(function(){
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
+  event.preventDefault();
 });
-
-//
-//
-alert("last line of scripts")
+    
