f40ae32644ed1b96056f2ab472eac8a306ed71e8
Garrett Price and Nhat Hoang <garrett@>
2016-08-10 13:59:38 -0700

    fixed the syntax error on the jquery function. Now that it would load variable to the output results.

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,10 @@ var divide = function(number1, number2) {return number1 / number2;};
 
 $(document).ready(function() {
   $("form#add").submit(function(event){
-    // event.preventDefault();
+    event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    // $("#output").text(result);
-  // });
+
   $("button#addbutton").click(function(){
     var result = add(number1, number2);
     $("#output").text(result);
@@ -31,5 +30,5 @@ $(document).ready(function() {
     $("#output").text(result);
   });
   event.preventDefault();
+  });
 });
-    
